3e4fc1a399bb2ddf446c2b210ffd0a0c0c81983e
VicenteMelRen <126625256+VicenteMelRen@users.noreply.github.com>
2023-06-09 11:54:04 +1000

    Monster
    AI assignment

---
+++ Assets/BattleSystem.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+public class BattleSystem : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public BattleHUD playerHUD;
+    public BattleHUD enemyHUD;
+
+    public Text dialogueText;
+
+    public BattleState state;
+
+    void Start()
+    {
+        state = BattleState.START;
+        StartCoroutine(SetupBattle());
+    }
+
+    IEnumerator SetupBattle()
+    {
+        GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerUnit = playerGO.GetComponent<Unit>();
+
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation); 
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogueText.text = "Love" + enemyUnit.unitName + "Coming...";
+        playerHUD.SetHUD(playerUnit);
+        enemyHUD.SetHUD(enemyUnit);
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn(); 
+
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+        dialogueText.text = "Bunny Attack Love!";
+        enemyHUD.SetHP(enemyUnit.currentHP);
+
+        yield return new WaitForSeconds(2f);
+
+        if(isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.ENEMYTURN;            
+            StartCoroutine(EnemyTurn());
+        }
+
+    }
+    
+    IEnumerator EnemyTurn()
+    {
+        dialogueText.text = enemyUnit.unitName + "attacks!";
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        playerHUD.SetHP(playerUnit.currentHP);
+
+        yield return new WaitForSeconds(1f);
+
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+    }
+
+    void EndBattle()
+    {
+        if(state == BattleState.WON)
+        {
+            dialogueText.text = "Bunny Won";
+        }
+        else if(state == BattleState.LOST)
+        {
+            dialogueText.text = "Love Won";
+        }
+    }
+    void PlayerTurn()
+    {
+        dialogueText.text = "ATTACK Love:";
+    }
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        StartCoroutine(PlayerAttack());
+    }
+}
+ 
